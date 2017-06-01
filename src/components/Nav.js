1f3a7bcdf64dac330a10f905c020c10985a9cb83
Matt Eves <mattheweves@outlook.com>
2017-06-01 08:21:23 -0700

    Initial App/Nav Component Setup

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div className="nav-container">
+        <div className="nav-logo">Kidfo</div>
+      </div>
+    );
+  }
+
+}
+
+export default Nav;
