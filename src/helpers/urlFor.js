eb86df6ade680a47f7528015548d2311139ac679
Matt Eves <mattheweves@outlook.com>
2017-08-02 15:46:32 -0700

    API UPDATE

--- src/helpers/urlFor.js
+++ src/helpers/urlFor.js
@@ -1,7 +1,7 @@
 
 const urlFor = (endpoint) => {
-    /*return 'https://kidfoapi.herokuapp.com/' + endpoint;*/
-    return 'http://localhost:3000/' + endpoint;
+    return 'https://kidfoapi.herokuapp.com/' + endpoint;
+    /*return 'http://localhost:3000/' + endpoint;*/
 };
 
 export default urlFor;
